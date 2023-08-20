c60a035a7e2dc15f9b104da0e550a052055c91e3
Rajat Gupta <93464321+27Rajat@users.noreply.github.com>
2023-08-20 11:31:33 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TrainSchedule from "./component/TrainScheduled";
+// import "./App.css";
+
+function App() {
+  return (
+    
+    <div className="App">
+      <TrainSchedule />
+    </div>
+  );
+}
+
+export default App;
