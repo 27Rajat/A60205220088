b03cc6e0f465d7dc9b47fa7993f837b43a50f6eb
Rajat Gupta <93464321+27Rajat@users.noreply.github.com>
2023-08-20 11:35:15 +0530

    Update TrainSchedule.js

--- TrainSchedule.js
+++ TrainSchedule.js
@@ -3,8 +3,13 @@ import TrainItem from "./TrainItem";
 
 const trainData = [
   { id: 1, departureTime: "09:00 AM", destination: "Gwalior" },
-  { id: 2, departureTime: "12:30 PM", destination: "Agra" }
-  // Add more train data
+  { id: 2, departureTime: "12:30 PM", destination: "Agra" },
+  { id: 1, departureTime: "08:00 AM", destination: "Delhi" },
+  { id: 2, departureTime: "11:30 PM", destination: "Indore" },
+  { id: 1, departureTime: "10:00 AM", destination: "Jhansi" },
+  { id: 2, departureTime: "01:30 PM", destination: "Mumbai" },
+  { id: 1, departureTime: "07:00 AM", destination: "Jaipur" },
+  { id: 2, departureTime: "05:30 PM", destination: "Kolkata" }
 ];
 
 function TrainSchedule() {
